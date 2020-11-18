94ba503324b83889e2d407797324a278f9b1d248
Tracy Chen <ychen151@cs.umd.edu>
2020-11-17 22:00:58 -0500

    combine auxiliary

--- auxplotsforBoxPlots.js
+++ auxplotsforBoxPlots.js
@@ -1,8 +1,9 @@
 window.onload = function() {
     d3.queue()  
       .defer(d3.json, 'data/all_selection_list.json')
+      .defer(d3.json, 'data/all_medal_stacked_bar.json')
       .defer(d3.json, 'data/all_age_error_bar.json')
-      .await(function(error, selectorData, ageData) {
+      .await(function(error, selectorData, medalData, ageData) {
         if (error) {
           console.error('Something went wrong: ' + error);
       }
@@ -46,26 +47,32 @@ window.onload = function() {
 
           }
 
+          //console.log(medalData);
+          //console.log(ageData);
 
-          ageErrorBar(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+          ageDotLine(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+          stackedBar(medalData[selectSeason.value][selectSport.value][selectEvent.value]);
           
           selectSeason.addEventListener('change', function() {
             setOptions(selectSport, Object.keys(selectorData[selectSeason.value]));
             setOptions(selectEvent, selectorData[selectSeason.value][selectSport.value]);
-            ageErrorBar(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+            ageDotLine(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+            stackedBar(medalData[selectSeason.value][selectSport.value][selectEvent.value]);
 
           });
 
           selectSport.addEventListener('change', function() {
             setOptions(selectEvent, selectorData[selectSeason.value][selectSport.value]);
-            ageErrorBar(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+            ageDotLine(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+            stackedBar(medalData[selectSeason.value][selectSport.value][selectEvent.value]);
             //console.log(selectSeason.value, selectSport.value, selectEvent.value);
           })
 
           selectEvent.addEventListener('change', function() {
             //console.log(selectSeason.value, selectSport.value, selectEvent.value);
             //console.log(data[selectSeason.value][selectSport.value][selectEvent.value]);
-            ageErrorBar(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+            ageDotLine(ageData[selectSeason.value][selectSport.value][selectEvent.value]);
+            stackedBar(medalData[selectSeason.value][selectSport.value][selectEvent.value]);
           })
 
         }
@@ -74,20 +81,20 @@ window.onload = function() {
 
 
 
-
-function ageErrorBar(data) {
+function ageDotLine(data) {
+    d3.select("#ageDotLinesvg").remove();
+    console.log(data);
     //e.g. [
 		//{"Year": 1996, "Medal": Gold, "minAge":17, "maxAge":31, "medianAge":21}, 
     //{"Year": 2000, "Medal": Silver, "minAge":17, "maxAge":31, "medianAge":21}] 
     
-    // clean previous drawing
-    d3.select("svg").remove();
     var margin = {top: 10, right: 30, bottom: 20, left: 50},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-    var svg = d3.select("#ageErrorBar")
+    var svg = d3.select("#ageDotLine")
       .append("svg")
+      .attr("id", "ageDotLinesvg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
       .append("g")
@@ -184,4 +191,95 @@ function ageErrorBar(data) {
           return x(d.Year); })
         .attr('cy', function(d) { return y(d.medianAge); })
   
+}
+
+
+
+function stackedBar(data) {
+  //console.log(data);
+  //d3.select("#stackedBar").remove();
+  d3.select("#stackedBarsvg").remove();
+  //e.g. [{Age: 24, Gold: 1, Silver: 0, Bronze: 0}, {Age: 25, Gold: 1, Silver: 0, Bronze: 0}]
+  age_ls = [];
+  medal_cnt_ls = [];
+  data.forEach((entry) => {
+    age_ls.push(entry['Age']);
+    tmp_sum = entry['Gold'] + entry['Silver'] + entry['Bronze'];
+    medal_cnt_ls.push(tmp_sum);
+  });
+  //sort age
+  age_ls.sort((a, b) => a - b);
+
+  //console.log(age_ls);
+  //find y range
+  maxMedal = d3.max(medal_cnt_ls);
+  
+  // get all ages
+  var groups = age_ls;
+  var subgroups = ['Gold', 'Silver', 'Bronze'];
+
+  var margin = {top: 10, right: 30, bottom: 20, left: 50},
+  width = 460 - margin.left - margin.right,
+  height = 400 - margin.top - margin.bottom;
+
+  var svg = d3.select("#stackedBar")
+    .append("svg")
+    .attr('id', 'stackedBarsvg')
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+  //axis
+  var x = d3.scaleBand()
+        .range([0, width])
+        .padding([0.2])
+        .domain(groups);
+
+  svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x).tickSizeOuter(0));
+  
+  var y = d3.scaleLinear()
+        .range([ height, 0 ])
+        .domain([0, maxMedal+1]);
+  
+  yAxisTicks = y.ticks()
+    .filter(tick => Number.isInteger(tick));
+
+  svg.append("g")
+    .call(d3.axisLeft(y).tickValues(yAxisTicks).tickFormat(d3.format('d')));
+
+  //check whether the mapping preserve the order
+  var color = d3.scaleOrdinal()
+    .domain(subgroups)
+    //medal colors
+    .range(['#FFD700', '#DCDCDC', '#ED9B4D']);
+  
+  //var stackedData = d3.stack()
+   // .keys(subgroups)(data);
+   //stack the data? --> stack per subgroup
+   var stackedData = d3.stack()
+   .keys(subgroups)
+   (data)
+
+  //console.log(data);
+  //console.log("the stacked data");
+  //console.log(stackedData);
+  
+  svg.append("g")
+    .selectAll("g")
+    // Enter in the stack data = loop key per key = group per group
+    .data(stackedData)
+    .enter().append("g")
+      .attr("fill", function(d) { return color(d.key); })
+      .selectAll("rect")
+      // enter a second time = loop subgroup per subgroup to add all rectangles
+      .data(function(d) { return d; })
+      .enter().append("rect")
+        .attr("x", function(d) { //console.log(d.data.Age); 
+          return x(d.data.Age); })
+        .attr("y", function(d) { return y(d[1]); })
+        .attr("height", function(d) { return y(d[0]) - y(d[1]); })
+        .attr("width",x.bandwidth())
 }
