a4b1eeea1b53a48c42b3e82e2de717cb7aca9350
Tracy Chen <ychen151@cs.umd.edu>
2020-11-29 13:52:20 -0500

    unit vis

--- prediction.js
+++ prediction.js
@@ -1,5 +1,4 @@
 //create year slider
-
 Array.range = (start, end) => Array.from({length: (end - start)}, (v, k) => k + start);
 var yearData = Array.range(12, 60);
 
@@ -86,49 +85,164 @@ window.onload = function() {
 )};
 */
 
-var data = {'Gold': 0.05, 'Silver': 0.15, 'Bronze':0.3, 'None':0.5};
-        updatePie(data);
+var data = {'Gold': 0.05, 'Silver': 0.15, 'Bronze':0.3, 'None':0.5, 'Accuracy': 0.9};
+updatePeople(data);
 
 
-function updatePie(data) {
-  var width = 450
-    height = 450
-    margin = 40
+function updatePeople(data) {
+  numVis = 100;
+  //circle size
+  r = 5;
+  peopleData = {};
+  uncertainCnt = numVis - Math.floor(numVis * data.Accuracy);
+  //console.log(peopleData);
+  sureCnt = Math.floor(numVis * data.Accuracy);
+  peopleData = {'Gold': Math.floor(sureCnt*data.Gold), 
+  'Silver': Math.floor(sureCnt*data.Silver),
+  'Bronze': Math.floor(sureCnt*data.Bronze),
+  'None': Math.floor(sureCnt*data.None),
+  'Uncertain': uncertainCnt
+};
+  console.log(peopleData);
 
-// The radius of the pieplot is half the width or half the height (smallest one). I subtract a bit of margin.
-var radius = Math.min(width, height) / 2 - margin
+  medalType = ['Gold', 'Silver', 'Bronze', 'None', 'Uncertain'];
+  goldArr = [];
+  silverArr = [];
+  bronzeArr = [];
+  noneArr = [];
+  uncertainArr = [];
 
-// append the svg object to the div called 'my_dataviz'
-var svg = d3.select("#pie-chart")
-  .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-  .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  for (var i in medalType) {
+    for (var j in [...Array(peopleData[medalType[i]]).keys()]) {
+      if (medalType[i] === 'Gold') {
+        goldArr.push('#FFD700');
+      }
+      else if (medalType[i] === 'Silver') {
+        silverArr.push('#DCDCDC');
+      }
+      else if (medalType[i] === 'Bronze') {
+        bronzeArr.push('#ED9B4D');
+      }
+      else if (medalType[i] === 'None') {
+        noneArr.push('black');
+      }
+      else if (medalType[i] === 'Uncertain') {
+        //no color for the circle
+        uncertainArr.push('white');
+      }
+    }
+  }
+  console.log(goldArr, silverArr, bronzeArr, noneArr, uncertainArr);
+  
 
-// set the color scale
-var color = d3.scaleOrdinal()
-  .domain(data)
-  .range(['#FFD700', '#DCDCDC', '#ED9B4D', '#000000'])
+  var width = 900;
+    height = 20;
+    margin = 0;
 
-// Compute the position of each group on the pie:
-var pie = d3.pie()
-  .value(function(d) {return d.value; })
-var data_ready = pie(d3.entries(data))
 
-// Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-svg
-  .selectAll('whatever')
-  .data(data_ready)
-  .enter()
-  .append('path')
-  .attr('d', d3.arc()
-    .innerRadius(0)
-    .outerRadius(radius)
-  )
-  .attr('fill', function(d){ return(color(d.data.key)) })
-  .attr("stroke", "black")
-  .style("stroke-width", "2px")
-  .style("opacity", 0.7)
+
+  for (var i in medalType) {
+        if (medalType[i] === 'Gold') {
+          if (goldArr.length > 0) {
+          medalContainer = d3.select("#unit-vis")
+            .append("svg")
+            .attr("id", 'gold')
+            .attr("width", width)
+            .attr("height", height);
+          medalContainer.selectAll("circle")
+            .data(goldArr)
+            .enter()
+            .append("circle")
+            .attr("cy", height/2)
+            .attr("cx", function(d, i) {
+                return (i+r)*2*r
+            })
+            .attr("r", r)
+            .style("fill", d=>d);
+          }
+          medalContainer.append("text")
+        }
+        else if (medalType[i] === 'Silver') {
+          if (silverArr.length > 0) {
+          medalContainer = d3.select("#unit-vis")
+            .append("svg")
+            .attr("id", 'silver')
+            .attr("width", width)
+            .attr("height", height);
+          medalContainer.selectAll("circle")
+            .data(silverArr)
+            .enter()
+            .append("circle")
+            .attr("cy", height/2)
+            .attr("cx", function(d, i) {
+                return (i+r)*2*r
+            })
+            .attr("r", r)
+            //.style("stroke", 'black')
+            .style("fill", d =>d);
+          }
+        }
+        else if (medalType[i] === 'Bronze') {
+          if (bronzeArr.length > 0) {
+          medalContainer = d3.select("#unit-vis")
+            .append("svg")
+            .attr("id", 'bronze')
+            .attr("width", width)
+            .attr("height", height);
+          medalContainer.selectAll("circle")
+            .data(bronzeArr)
+            .enter()
+            .append("circle")
+            .attr("cy", height/2)
+            .attr("cx", function(d, i) {
+                return (i+r)*2*r
+            })
+            .attr("r", r)
+            //.style("stroke", 'black')
+            .style("fill", d=>d);
+          }
+        }
+        else if (medalType[i] === 'None') {
+          if (noneArr.length > 0) {
+          medalContainer = d3.select("#unit-vis")
+            .append("svg")
+            .attr("id", 'none')
+            .attr("width", width)
+            .attr("height", height);
+          medalContainer.selectAll("circle")
+            .data(noneArr)
+            .enter()
+            .append("circle")
+            .attr("cy", height/2)
+            .attr("cx", function(d, i) {
+                return (i+r)*2*r
+            })
+            .attr("r", r)
+            //.style("stroke", 'black')
+            .style("fill", d=>d);
+          }
+        }
+        else if (medalType[i] === 'Uncertain') {
+          if (uncertainArr.length > 0) {
+          medalContainer = d3.select("#unit-vis")
+            .append("svg")
+            .attr("id", 'uncertain')
+            .attr("width", width)
+            .attr("height", height);
+          medalContainer.selectAll("circle")
+            .data(uncertainArr)
+            .enter()
+            .append("circle")
+            .attr("class", 'uncertain')
+            .attr("cy", height/2)
+            .attr("cx", function(d, i) {
+                return (i+r)*2*r
+            })
+            .attr("r", r)
+            .style("stroke", 'black')
+            .style("fill", d => d);
+        }
+      }
+    }
 
 }
